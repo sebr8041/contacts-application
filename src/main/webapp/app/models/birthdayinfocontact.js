3ecd8701a833b3deeab654cc6a2c091e0556ce7b
Sebastian Brodersen <brodersen.sebastian@googlemail.com>
2016-11-03 22:42:42 +0100

    Clean project

--- src/main/webapp/app/models/birthdayinfocontact.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var BirthdayInfoContact = (function () {
-    function BirthdayInfoContact() {
-    }
-    return BirthdayInfoContact;
-}());
-exports.BirthdayInfoContact = BirthdayInfoContact;
-//# sourceMappingURL=birthdayinfocontact.js.map
--- src/main/webapp/app/test.component.ts
+++
@@ -1,9 +0,0 @@
-import{Component} from "@angular/core";
-
-@Component({
-    selector: 'contacts-application',
-    template: 'This is Test'
-})
-export class TestComponent{
-
-} 
