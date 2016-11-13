ebbfb44e6ec240d4d65198e0348b21e11fe4b776
Sebastian Brodersen <brodersen.sebastian@googlemail.com>
2016-11-13 15:57:58 +0100

    Kategorie löschen #5

--- src/main/webapp/app/category/listcategory.component.js
+++ src/main/webapp/app/category/listcategory.component.js
@@ -10,28 +10,80 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require("@angular/core");
 var category_service_1 = require('../service/category.service');
+var bootstrap_1 = require('angular2-modal/plugins/bootstrap');
+var angular2_notifications_1 = require('angular2-notifications');
+var angular2_modal_1 = require('angular2-modal');
 var ListCategoryComponent = (function () {
     /**
      * Autowire contactService
      */
-    function ListCategoryComponent(categoryService) {
+    function ListCategoryComponent(categoryService, modal, notificationService, overlay, vcRef) {
         var _this = this;
         this.categoryService = categoryService;
+        this.modal = modal;
+        this.notificationService = notificationService;
         /**
          * saves all categorys from backend.
          */
         this.categorys = [];
+        // container or modals.
+        overlay.defaultViewContainer = vcRef;
         // load all categorys 
         this.categoryService.getAll().subscribe(function (categorys) {
             _this.categorys = categorys;
         });
     }
+    /**
+     * Handler delete category.
+     */
+    ListCategoryComponent.prototype.deleteCategory = function (id) {
+        var _this = this;
+        // find contact 
+        this.categoryService.find(id).subscribe(function (category) {
+            console.log("dfsf");
+            // user should confirm deleting a contact.
+            _this.modal.confirm()
+                .size('sm')
+                .isBlocking(true)
+                .showClose(true)
+                .keyboard(27)
+                .title('Kontakt löschen')
+                .body('Kontakt "' + category.name + '" wirklich löschen?')
+                .okBtn('Löschen')
+                .okBtnClass('btn btn-danger')
+                .cancelBtn('Abbrechen')
+                .cancelBtnClass('btn btn-default')
+                .open()
+                .then(function (result) { return result.result; })
+                .then(function (result) {
+                _this.categoryService.remove(category.id).subscribe(function (result) {
+                    _this.deleteContactFromLocaleArray(category.id);
+                    _this.notificationService.success("Erfolg!", 'Kategorie  "' + category.name + '" gelöscht!');
+                }, function (error) {
+                    _this.notificationService.error("Fehlgeschlagen!", 'Die Kategorie hat noch Kontakte. Somit kann diese nicht gelöscht werden.');
+                });
+            }, function (result) {
+                // fail => nothing happens. user canceled action
+            });
+        });
+    };
+    /**
+      * deleteing a category from the locale array (no need to reload complete contacts)
+      */
+    ListCategoryComponent.prototype.deleteContactFromLocaleArray = function (id) {
+        var _this = this;
+        this.categorys.forEach(function (category, index) {
+            if (category.id === id) {
+                _this.categorys.splice(index, 1);
+            }
+        });
+    };
     ListCategoryComponent = __decorate([
         core_1.Component({
             selector: 'contacts-application',
             templateUrl: 'app/category/listcategory.component.html'
         }), 
-        __metadata('design:paramtypes', [category_service_1.CategoryService])
+        __metadata('design:paramtypes', [category_service_1.CategoryService, bootstrap_1.Modal, angular2_notifications_1.NotificationsService, angular2_modal_1.Overlay, core_1.ViewContainerRef])
     ], ListCategoryComponent);
     return ListCategoryComponent;
 }());
--- src/main/webapp/app/category/listcategory.component.ts
+++ src/main/webapp/app/category/listcategory.component.ts
@@ -1,12 +1,14 @@
-import{Component} from "@angular/core";
+import { Component, ViewContainerRef, ViewEncapsulation } from "@angular/core";
 import { CategoryService } from '../service/category.service';
 import { ICategory } from '../models/category';
-
+import { Modal } from 'angular2-modal/plugins/bootstrap';
+import { NotificationsService } from 'angular2-notifications';
+import { Overlay } from 'angular2-modal';
 @Component({
     selector: 'contacts-application',
     templateUrl: 'app/category/listcategory.component.html'
 })
-export class ListCategoryComponent{
+export class ListCategoryComponent {
 
 
     /**
@@ -14,17 +16,65 @@ export class ListCategoryComponent{
      */
     private categorys: ICategory[] = [];
 
-  
+
     /**
      * Autowire contactService
      */
-    constructor(private categoryService: CategoryService) {
+    constructor(private categoryService: CategoryService,
+        public modal: Modal,
+        private notificationService: NotificationsService,
+        overlay: Overlay,
+        vcRef: ViewContainerRef, ) {
+        // container or modals.
+        overlay.defaultViewContainer = vcRef;
         // load all categorys 
         this.categoryService.getAll().subscribe((categorys) => {
             this.categorys = categorys;
         });
-
     }
 
+    /**
+     * Handler delete category.
+     */
+    public deleteCategory(id: string) {
+        // find contact 
+        this.categoryService.find(id).subscribe((category) => {
+            console.log("dfsf");
+            // user should confirm deleting a contact.
+            this.modal.confirm()
+                .size('sm')
+                .isBlocking(true)
+                .showClose(true)
+                .keyboard(27)
+                .title('Kontakt löschen')
+                .body('Kontakt "' + category.name + '" wirklich löschen?')
+                .okBtn('Löschen')
+                .okBtnClass('btn btn-danger')
+                .cancelBtn('Abbrechen')
+                .cancelBtnClass('btn btn-default')
+                .open()
+                .then((result) => result.result)
+                .then((result) => {
+                    this.categoryService.remove(category.id).subscribe((result) => {
+                        this.deleteContactFromLocaleArray(category.id);
+                        this.notificationService.success("Erfolg!", 'Kategorie  "' + category.name + '" gelöscht!');
+                    }, (error) => {
+                        this.notificationService.error("Fehlgeschlagen!", 'Die Kategorie hat noch Kontakte. Somit kann diese nicht gelöscht werden.');
+                    });
+                }, (result) => {
+                    // fail => nothing happens. user canceled action
+                })
+        });
+    }
 
+    /**
+      * deleteing a category from the locale array (no need to reload complete contacts)
+      */
+    private deleteContactFromLocaleArray(id: string) {
+        this.categorys.forEach((category, index) => {
+            if (category.id === id) {
+                this.categorys.splice(index, 1);
+            }
+        });
+    }
 } 
